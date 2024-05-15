539ca65c750854f3dd28f7678912ff77d6cd9473
Aine-dickson <ainedixon01@gmail.com>
2024-05-15 12:12:10 +0300

    init

---
+++ datc/target/debug/deps/datc-25d8e8d2b60e5034.d
@@ -0,0 +1,5 @@
+/home/ainedeveloper/Projects/Rust-Apps/DATC/datc/target/debug/deps/libdatc-25d8e8d2b60e5034.rmeta: src/main.rs
+
+/home/ainedeveloper/Projects/Rust-Apps/DATC/datc/target/debug/deps/datc-25d8e8d2b60e5034.d: src/main.rs
+
+src/main.rs:
---
+++ datc/target/debug/deps/datc-6f0486b29d881695.d
@@ -0,0 +1,5 @@
+/home/ainedeveloper/Projects/Rust-Apps/DATC/datc/target/debug/deps/libdatc-6f0486b29d881695.rmeta: src/main.rs
+
+/home/ainedeveloper/Projects/Rust-Apps/DATC/datc/target/debug/deps/datc-6f0486b29d881695.d: src/main.rs
+
+src/main.rs:
